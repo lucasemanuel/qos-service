53b18f9e6316505cb01a361ff98a5506d98ff2a7
Lucas Emanuel <lucaxemanuel@gmail.com>
2018-11-13 15:36:05 -0300

    Adicionando configurações no classificador

--- etc/init.d/classificador.sh
+++ etc/init.d/classificador.sh
@@ -9,3 +9,6 @@ iptables -t mangle -A FORWARD -p tcp -m multiport --ports 25,110,995 -j CLASSIFY
 
 # trafego HTTP entrando
 iptables -t mangle -A FORWARD -o eth0 -p tcp --sport 80 -j CLASSIFY --set-class 1:30
+
+# iptables -t mangle -A FORWARD -d 10.0.0.1 -p tcp --sport 110 -j CLASSIFY --set-class 1:65
+# iptables -t mangle -A FORWARD -d 10.0.0.0/8 -p tcp --sport 110 -j CLASSIFY --set-class 1:70
--- etc/init.d/controle_banda.sh
+++ etc/init.d/controle_banda.sh
@@ -11,6 +11,9 @@ tc qdisc add dev eth0 root handle 1:0 htb default 40
 # esta classe estabelece que a interface de rede eth0 funcionará a 100 Mb/s
 tc class add dev eth0 parent 1:0 classid 1:1 htb rate 100mbit
 
+# A classe 1:10 é filha da classe 1:1. Ela permitirá um tráfego de 100 Kb/s. 
+# Caso as outras classes estejam sem tráfego, esta classe poderá emprestar um pouco do link que sobra nas outras, podendo chegar a 300 Kb/s. 
+# Esta classe tem prioridade 0, ou seja, todas as outras irão parar para o seu tráfego passar (caso isso seja necessário). 
 tc class add dev eth0 parent 1:1 classid 1:10 htb rate 100kbit  ceil 300kbit  prio 0   # DNS / ACK / SYN / FIN
 tc class add dev eth0 parent 1:1 classid 1:20 htb rate 500kbit  ceil 800kbit  prio 1   # E-mail
 tc class add dev eth0 parent 1:1 classid 1:30 htb rate 1200kbit ceil 1600kbit prio 2   # HTTP / HTTPS
